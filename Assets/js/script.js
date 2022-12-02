eb5208f8467da414826c251cad05baafc23a45ee
Andy Abebaw <andyabebaw1@gmail.com>
2022-12-01 20:38:23 -0800

    add all the times to the page, add styling based on tense, add comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,50 +1,82 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var timeData = [];
+var timeIDs = ["hour-9", "hour-10", "hour-11", "hour-12", "hour-1", "hour-2", "hour-3", "hour-4", "hour-5"]
+var timeName = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
+var timeValues = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+class TimeData {
+  constructor(timeID, timeName, timeValue) {
+    this.timeID = timeID;
+    this.timeName = timeName;
+    this.timeValue = timeValue;
+  }
+  getTense() {
+    var hours = new Date().getHours();
+    if (hours < this.timeValue) {
+      return "past";
+    } else if (hours > this.timeValue) {
+      return "future";
+    } else {
+      return "present";
+    }
+  }
+}
+
+// Fills time Data with all the TimeData objects for each work hour
+for (let i = 0; i < timeIDs.length; i++) {
+  var timeElement = new TimeData(timeIDs[i], timeName[i], timeValues[i]);
+  timeData.push(timeElement);
+}
+
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  function setTextInputOnPageLoad(){
-    $(".description").each(function () {
-      var textInput = this;
-      var text = localStorage.getItem(textInput.parentElement.id)
-      textInput.innerHTML = text;
-    })
-  }
-
-  function addSaveButtonEventListeners() {
-    $(".saveBtn").each(function () {
-      var button = this;
-      var parentID = button.parentElement.id;
-      button.addEventListener("click", function () {
-        event.preventDefault();
-        var textInput = $("#"+parentID).children('textarea')
-        localStorage.setItem(parentID, textInput.val())
-        console.log(textInput.val());
-      });
-    })
-  }
-
+  addTimeElements()
   setTextInputOnPageLoad()
   addSaveButtonEventListeners()
   setTodaysDate()
 });
+
+// Creates Time Element Html Code and adds it to the webpage
+function addTimeElements(){
+  timeData.forEach(function (time) {
+
+    var timeElement = createTimeElement(time.timeID, time.timeName, time.getTense());
+    $(".container-lg").append(timeElement);
+  });
+}
+
+// Converts TimeData Object to Html Code
+function createTimeElement(timeID, timeName, tense) {
+  return ('<div id="' + timeID + '" class="row time-block ' + tense + '">' +
+    '<div class="col-2 col-md-1 hour text-center py-3">' + timeName + '</div>' +
+    '<textarea class="col-8 col-md-10 description" rows="3"> </textarea>' +
+    '<button class="btn saveBtn col-2 col-md-1" aria-label="save">' +
+    '<i class="fas fa-save" aria-hidden="true"></i>' +
+    '</button>' +
+    '</div>')
+}
+
+// Sets Time Elements Text based on LocalStorage
+function setTextInputOnPageLoad() {
+  $(".description").each(function () {
+    var textInput = this;
+    var text = localStorage.getItem(textInput.parentElement.id)
+    textInput.innerHTML = text;
+  })
+}
+
+// Adds event listeners to all the save buttons 
+function addSaveButtonEventListeners() {
+  $(".saveBtn").each(function () {
+    var button = this;
+    var parentID = button.parentElement.id;
+    button.addEventListener("click", function () {
+
+      event.preventDefault();
+      var textInput = $("#" + parentID).children('textarea')
+      localStorage.setItem(parentID, textInput.val())
+    });
+  })
+}
 
 // Code to display the current date in the header of the page.
 function setTodaysDate() {
@@ -61,4 +93,3 @@ function setTodaysDate() {
   today = day + ', ' + mm + ' ' + dd;
   dateEl.text(today);
 }
-
